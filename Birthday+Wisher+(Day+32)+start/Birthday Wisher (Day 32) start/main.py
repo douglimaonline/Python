0b4f4deee908216ffe222a3471c5205bdde0ab1d
Douglas <douglimaonline@gmail.com>
2023-08-23 17:12:28 -0300

    23day project update

--- Birthday+Wisher+(Day+32)+start/Birthday Wisher (Day 32) start/main.py
+++ Birthday+Wisher+(Day+32)+start/Birthday Wisher (Day 32) start/main.py
@@ -3,8 +3,7 @@ import smtplib
 my_email = "douglasbarbozadev@gmail.com"
 password = "xqitmvsnarwtwvdv"
 
-connection = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-# connection.starttls()
-connection.login(user=my_email, password=password)
-connection.sendmail(from_addr=my_email, to_addrs="lima_on-line@hotmail.com", msg="Hello")
-connection.close()
+with smtplib.SMTP_SSL("smtp.gmail.com", 465) as connection:
+    connection.login(user=my_email, password=password)
+    connection.sendmail(from_addr=my_email, to_addrs="douglimaonline@gmail.com",
+                        msg="Subject:Automatic Hello\n\nMessenger send by python smtplib by douglas_barboza_dev")
