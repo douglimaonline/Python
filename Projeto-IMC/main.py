ba03c1988df6dc3acb73a5865cd565f445dea2cf
Douglas <douglimaonline@gmail.com>
2023-10-22 18:48:47 -0300

    adding Projeto IMC (faculdade)

---
+++ Projeto-IMC/main.py
@@ -0,0 +1,14 @@
+
+# 1 - Criar um programa que faça calculo do IMC;
+
+# Fórmula IMC = Peso ÷ (Altura × Altura)
+
+altura = float(input("Digite sua altura (em metros): "))
+peso = float(input("Digite seu peso (em kg): "))
+
+imc = peso / (altura * altura)
+
+print(f"Seu IMC é {imc:.2f}.")
+
+
+# 2 Criar um relatório no final da atividade.
